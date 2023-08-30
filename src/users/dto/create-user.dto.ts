2ac4aa6d4ed02e744f68478a2bbca7e6849c5148
Jestin Mounts <jkmounts@gmail.com>
2023-08-30 14:59:36 +0000

    add validation pipe

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
   @ApiProperty()
   email: string;
 
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(50)
   @ApiProperty()
   name: string;
 }
