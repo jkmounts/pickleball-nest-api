8188eb43c7306426d502ef3b60097ff32c3a80ff
Jestin Mounts <jkmounts@gmail.com>
2023-09-04 19:35:17 +0000

    add court tests

--- src/courts/courts.controller.spec.ts
+++ src/courts/courts.controller.spec.ts
@@ -4,8 +4,12 @@ import { CourtsService } from './courts.service';
 import { PrismaService } from 'nestjs-prisma';
 import { prismaMock } from 'src/singleton';
 
+import { courtEntities } from './mocks/courts.mock';
+const mockCourt = courtEntities[0];
+
 describe('CourtsController', () => {
   let controller: CourtsController;
+  let service: CourtsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,9 +24,19 @@ describe('CourtsController', () => {
     }).compile();
 
     controller = module.get<CourtsController>(CourtsController);
+    service = module.get<CourtsService>(CourtsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('findOne', () => {
+    it('should call CourtsService.findOne', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(mockCourt);
+      await controller.findOne(1, 1);
+      expect(service.findOne).toBeCalledTimes(1);
+      expect(service.findOne).toBeCalledWith(1, 1);
+    });
   });
 });
---
+++ src/courts/mocks/courts.mock.ts
@@ -0,0 +1,14 @@
+const courtEntities = [
+  {
+    courtNumber: 1,
+    venueId: 1,
+    name: 'Challenge Court',
+  },
+  {
+    courtNumber: 2,
+    venueId: 1,
+    name: null,
+  },
+];
+
+export { courtEntities };
