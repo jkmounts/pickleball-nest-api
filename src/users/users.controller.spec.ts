9c0ad6b485f0f64d596176834d0127e91eb1fd08
Jestin Mounts <jkmounts@gmail.com>
2023-09-04 13:09:37 +0000

    UsersController unit tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -6,6 +6,7 @@ import { PrismaService } from 'nestjs-prisma';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let service;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +18,63 @@ describe('UsersController', () => {
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should call UsersService.create', async () => {
+      jest.spyOn(service, 'create').mockResolvedValue('test');
+      const userDto = {
+        name: 'Jim',
+        email: 'jim@example.com',
+      };
+      await controller.create(userDto);
+      expect(service.create).toBeCalledTimes(1);
+      expect(service.create).toBeCalledWith(userDto);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call UsersService.findAll', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue('test');
+      await controller.findAll();
+      expect(service.findAll).toBeCalledTimes(1);
+      expect(service.findAll).toBeCalledWith();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should call UsersService.findOne', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue('test');
+      await controller.findOne(1);
+      expect(service.findOne).toBeCalledTimes(1);
+      expect(service.findOne).toBeCalledWith(1);
+    });
+  });
+
+  describe('update', () => {
+    it('should call UsersService.update', async () => {
+      const userDto = {
+        name: 'Jim',
+        email: 'jim@example.com',
+      };
+      jest.spyOn(service, 'update').mockResolvedValue('test');
+      await controller.update(1, userDto);
+      expect(service.update).toBeCalledTimes(1);
+      expect(service.update).toBeCalledWith(1, userDto);
+    });
+  });
+
+  describe('remove', () => {
+    it('should call UsersService.remove', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue('test');
+      await controller.remove(1);
+      expect(service.remove).toBeCalledTimes(1);
+      expect(service.remove).toBeCalledWith(1);
+    });
   });
 });
